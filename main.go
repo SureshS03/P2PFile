16f019db6f3aa91fa57e1a7c29350864ebc95421
Suresh S <suthamani51@gmail.com>
2025-05-31 14:18:53 +0530

    added wonrg notes

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 //method 1 is zip the chunks and send it
 // method 2 is send the chunks one by one not recommended tho
 
+//cant send large zip more then 20mb, have to try another method
+//compress chuck and send or use go routines 
+
 func main() {
 	//splitFile("test.mp4")
 	args := os.Args
