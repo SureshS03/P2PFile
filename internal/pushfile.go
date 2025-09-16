4b2d75d18c8d1885706f0c048670ff8abfedf5e6
SureshS03 <suthamani51@gmail.com>
2025-09-16 20:14:55 +0530

    clr and err handled

--- internal/pushfile.go
+++ internal/pushfile.go
@@ -7,21 +7,20 @@ import (
 
 func PushFile(id string, to string) error {
 	start := time.Now()
-	fmt.Println("Pushing file", id)
-	fmt.Println("to is",to)
+	CrrPrinter("Pushing file: " + id)
+	CrrPrinter("to is: " + to)
 	metadata, err := getMetaData("MetaData.json")
 	if err != nil {
 		return fmt.Errorf("bro cant access MetaData File %s", err)
 	}
 	err = isFileExits(&metadata, &id)
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
 	err = sendMail(&id, &metadata, to)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
-	fmt.Println("Encoding and send took:", time.Since(start))
+	CrrPrinter("DONE, Encoding and send took: " + fmt.Sprint(time.Since(start)))
 	return nil
 }
